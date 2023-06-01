4b2d563d7b0d25fb5206818a5f368fdb12a6dfaa
RaulSales <raulsahe@protonmail.com>
2023-06-01 19:49:34 +0200

    replace

--- angular-Raul/examen_segona_eva/segona_evaluacio/src/app/main/content/content.component.ts
+++ angular-Raul/examen_segona_eva/segona_evaluacio/src/app/main/content/content.component.ts
@@ -6,31 +6,29 @@ import { Images } from 'src/app/views/view-one/images';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-  /* Grupo 1 */
-  public one: Images = {
-    image: ['https://i.blogs.es/2cc78a/ordenstarwars/840_560.jpg'],
-    groupText: 'Grupo Tres',
-    clase: 'image-container'
-  }
-
-  /* Grupo 2 */
-  public two: Images = {
-    image: ['https://www.lavanguardia.com/files/image_948_465/uploads/2020/05/04/5fa922920d3b5.png'],
-    groupText: 'Grupo Dos',
-    clase: 'image-container2'
-  }
-
-  /* Grupo 3 */
-  public three: Images = {
-    image: ['https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png'],
-    groupText: 'Grupo Tres',
-    clase: 'image-container3'
-  }
+  
 
   @Input() image: string[] = [];
   @Input() text: string = "";
   @Input() clase: string = "";
   @Input() type = "";
 
-  
+  ChangeSize(){
+
+  }
+
+  ImageHide(){
+
+
+  }
+
+  previous() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.previousClick.emit();
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.nextClick.emit();
+  }
 }
--- angular-Raul/examen_segona_eva/segona_evaluacio/src/app/views/view-one/view-one.component.ts
+++ angular-Raul/examen_segona_eva/segona_evaluacio/src/app/views/view-one/view-one.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
 import { ImageGroup } from 'src/app/models/content';
+import { Images } from './images';
 @Component({
   selector: 'app-view-one',
   templateUrl: './view-one.component.html',
   styleUrls: ['./view-one.component.css']
 })
 export class ViewOneComponent {
-  imageGroups: ImageGroup[] = [
-    {
-      imageUrl: 'https://i.blogs.es/2cc78a/ordenstarwars/840_560.jpg',
-      caption: 'Grupo Uno',
-    },
-    {
-      imageUrl:
-        'https://www.lavanguardia.com/files/image_948_465/uploads/2020/05/04/5fa922920d3b5.png',
-      caption: 'Grupo Dos',
-    },
-    {
-      imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png',
-      caption: 'Grupo Tres',
-    },
-    
-  ];
+  /* Grupo 1 */
+  public one: Images = {
+    image: ['https://i.blogs.es/2cc78a/ordenstarwars/840_560.jpg'],
+    groupText: 'Grupo Tres',
+    clase: 'image-container'
+  }
+
+  /* Grupo 2 */
+  public two: Images = {
+    image: ['https://www.lavanguardia.com/files/image_948_465/uploads/2020/05/04/5fa922920d3b5.png'],
+    groupText: 'Grupo Dos',
+    clase: 'image-container2'
+  }
+
+  /* Grupo 3 */
+  public three: Images = {
+    image: ['https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png'],
+    groupText: 'Grupo Tres',
+    clase: 'image-container3'
+  }
 
   currentGroupIndex: number = 0;
   currentImageGroup: ImageGroup;
@@ -35,8 +40,7 @@ export class ViewOneComponent {
     this.currentImageGroup = this.imageGroups[this.currentGroupIndex];
   }
 
-  nextGroup() {
-    this.currentGroupIndex = (this.currentGroupIndex + 1) % this.imageGroups.length;
-    this.currentImageGroup = this.imageGroups[this.currentGroupIndex];
-  }
+  previousImage(){}
+
+  nextImage(){}
 }
